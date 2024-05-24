bc4cee07eb2c2aa51d6cb5b26f6dd3a8e9fcce2e
Rain-coe <cahein22@coe.edu>
2024-05-24 11:12:57 -0500

    Fixed python 3d

--- python3d.py
+++ python3d.py
@@ -1,17 +1,15 @@
-#Python 3d plot
 import matplotlib.pyplot as plt
-# from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
-def plot3d(file):
-    infile = open(file)
+def plot3d(fileIn):
+    infile = open(fileIn)
     fig=plt.figure()
     ax=fig.add_subplot(projection='3d')
     for line in infile:
-        posX=float((line.strip()).split()[9])
-        posY=float((line.strip()).split()[10])
-        posZ=float((line.strip()).split()[11])
-        ax.scatter(posX,posY,posZ,marker=1)
+        xs=float((line.strip()).split()[9])
+        ys=float((line.strip()).split()[10])
+        zs=float((line.strip()).split()[11])
+        ax.scatter(xs,ys,zs)
     ax.set_xlabel('X position')
     ax.set_ylabel('Y position')
     ax.set_zlabel('Z position')
