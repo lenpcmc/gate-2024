361ae01a42f260dfe54b24d5d2f1bcb710ee2e55
Walker <wllaw23@coe.edu>
2024-06-03 16:53:45 -0500

    curvefit yay & end of day 6/3/2024

--- dosimeterAI/curvefit.py
+++ dosimeterAI/curvefit.py
@@ -2,28 +2,23 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit
+from scipy.integrate import quad
 
-X = np.random.random(size=(100))*10
+X, Y = pd.read_csv('/home/vgate/src/gate-2024/mac/l;ajsdfl;a') #incoming photon data
 
-m = 522.35453354364334153643345463543463
-C = .3522353344643541463
-Y = m * X ** 2 - C * X ** 3 + C * X ** 4
-
+plt.title('eDep vs photons emitted')
 plt.scatter(X,Y)
 
 plt.show()
 
-def curve(X,m,C):
-  return(m * X ** 2 - C * X ** 3 + C * X ** 4)
+def landau_dist(a,b,c):
+    return a*(np.exp(b/X)/(X*(c+X)))
 
-curve_fit(curve,X,Y,p0=[5646,1144],maxfev=100000)
+curve_fit(landau_dist,X,Y,p0=[5646,1144],maxfev=100000)
 
+p_par,p_cov = curve_fit(landau_dist,X,Y,p0=[10,5],maxfev=10000)
 
-p_par,p_cov = curve_fit(curve,X,Y,p0=[10,5],maxfev=10000)
-p_par
-
-Y_fit = curve(X,5.22354534e+02,3.52235334e-01)
-Y_fit
+Y_fit = landau_dist(X,5.22354534e+02,3.52235334e-01)
 
 plt.scatter(X,Y)
 plt.scatter(X,Y_fit,color='red')
